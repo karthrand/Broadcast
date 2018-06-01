ca087684624daaec4fc29b83533f322753dc1e91
Karthrand <735867550@qq.com>
2018-06-01 12:59:36 +0800

    完善网络类型判定

--- app/src/main/java/com/oude/broadcast/MainActivity.java
+++ app/src/main/java/com/oude/broadcast/MainActivity.java
@@ -9,23 +9,41 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.widget.Toast;
+import android.net.Network;
 
 public class MainActivity extends AppCompatActivity 
 {
+    /** 
+    //动态广播接收器第一步：声明两个网络管理对象和接收的意图类型
     private IntentFilter intentFilter;
     private NetworkChangeReceiver networkChangeReceiver;
+    **/
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        /**
+        //动态广播接收器第三步：实例创建、确定接收广播类型和注册广播
         intentFilter =new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver =new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver,intentFilter);
-        
+        **/
     }
     
+    /**
+     //动态广播接收器第四步:取消广播接收器的注册
+     //最后一步是去清单文件添加权限
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+    
+    
+    //动态广播接收器第二步：创建对应的广播接收器的内部类,并进行基本的网络链接判断
     class NetworkChangeReceiver extends BroadcastReceiver
     {
 
@@ -34,11 +52,17 @@ public class MainActivity extends AppCompatActivity
         {
             ConnectivityManager connectivityManager =(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            
             if (networkInfo != null && networkInfo.isAvailable()){
                 Toast.makeText(p1,"网络已连接!",Toast.LENGTH_SHORT).show();
+                
             } else{
+               
                 Toast.makeText(p1,"网络不可用!",Toast.LENGTH_SHORT).show();
             }
         }
     }
+    
+    **/
+    
 }
--- app/src/main/java/com/oude/broadcast/StaticNetworkChangeReceiver.java
+++ app/src/main/java/com/oude/broadcast/StaticNetworkChangeReceiver.java
@@ -5,20 +5,84 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.widget.Toast;
+import android.os.Build;
+import android.net.Network;
 
+
+//静态广播接收器第一步：创建对应的接收器类，并实现onRwceive方法
 public class StaticNetworkChangeReceiver extends BroadcastReceiver
 {
+    private Integer networkType;
+    private String networkName;
+    private boolean networkConnected;
 
     @Override
     public void onReceive(Context p1, Intent p2)
     {
-        ConnectivityManager connectivityManager =(ConnectivityManager) p1.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isAvailable()){
-            Toast.makeText(p1,"外部:网络已连接!",Toast.LENGTH_SHORT).show();
+        //调用判定网络是否连接方法
+        networkConnected = isNetworkAvailable(p1);
+        
+        if (networkConnected){
+            ConnectivityManager connectivityManager =(ConnectivityManager) p1.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            networkType = networkInfo.getType();
+            networkName = networkInfo.getSubtypeName();
+            
+            switch (networkType){
+                case connectivityManager.TYPE_BLUETOOTH:
+                    networkName = "蓝牙热点";
+                    break;
+                case connectivityManager.TYPE_MOBILE:
+                    networkName = "手机流量";
+                    break;
+                case connectivityManager.TYPE_WIFI:
+                    networkName = "WIFI";
+                    break;
+                case connectivityManager.TYPE_VPN:
+                    networkName = "VPN";
+                    break;
+                default:
+                    networkName="";  
+            }
+            
+            Toast.makeText(p1,networkName +"网络已连接!",Toast.LENGTH_SHORT).show();
         } else{
-            Toast.makeText(p1,"外部:网络不可用!",Toast.LENGTH_SHORT).show();
+            Toast.makeText(p1,"网络已断开!",Toast.LENGTH_SHORT).show();
         }
+    }
+    
+    
+    //判定网络是否已连接
+    public static boolean isNetworkAvailable(Context context) {
+        if (context == null) {
+            return false;
+        }
+        ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        //新版本调用方法获取网络状态
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Network[] networks = connectivity.getAllNetworks();
+            NetworkInfo networkInfo;
+            for (Network mNetwork : networks) {
+                networkInfo = connectivity.getNetworkInfo(mNetwork);
+                if (networkInfo.getState().equals(NetworkInfo.State.CONNECTED)) {
+                    return true;
+                }
+            }
+        } else {
+            //否则调用旧版本方法
+            if (connectivity != null) {
+                NetworkInfo[] info = connectivity.getAllNetworkInfo();
+                if (info != null) {
+                    for (NetworkInfo anInfo : info) {
+                        if (anInfo.getState() == NetworkInfo.State.CONNECTED) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     
